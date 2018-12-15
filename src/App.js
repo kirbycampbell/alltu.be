daa6c4321cac76d8544058d500745fdd36ef642a
kirbycampbell <jkirbycampbell@gmail.com>
2018-12-14 21:01:58 -0800

    Working with db

--- src/App.js
+++ src/App.js
@@ -1,65 +1,87 @@
 import React, { Component } from "react";
 import "./App.css";
 import Amplify, { Storage, API, graphqlOperation } from "aws-amplify";
-import aws_exports from "./aws-exports";
+import config from "./aws-exports";
 import { withAuthenticator, S3Album } from "aws-amplify-react";
-Amplify.configure(aws_exports);
+Amplify.configure(config);
 Storage.configure({ level: "private" });
 
-const listTodos = `query listTodos {
-  listTodos{
-    items{
-      id
-      name
-      description
+const query = `
+  query {
+    listVideos {
+      items {
+        id title
+      }
     }
-  }
-}`;
+  }`;
 
-const addTodo = `mutation createTodo($name:String! $description: String!) {
-  createTodo(input:{
-    name:$name
-    description:$description
-  }){
-    id
-    name
-    description
-  }
-}`;
+//const listTodos = `query listTodos {
+// listTodos{
+//   items{
+//     id
+//      name
+//      description
+//    }
+//  }
+//}`;
+
+//const addTodo = `mutation createTodo($name:String! $description: String!) {
+// createTodo(input:{
+//    name:$name
+//    description:$description
+//  }){
+//   id
+//   name
+//   description
+//  }
+//}`;
 
 class App extends Component {
-  uploadFile = evt => {
-    const file = evt.target.files[0];
-    const name = file.name;
+  state = {
+    videos: []
+  };
 
-    Storage.put(name, file).then(() => {
-      this.setState({ file: name });
+  async componentDidMount() {
+    const data = await API.graphql(graphqlOperation(query));
+    this.setState({
+      videos: data.data.listVideos.items
     });
-  };
-
-  todoMutation = async () => {
-    const todoDetails = {
-      name: "Party tonight!",
-      description: "Amplify CLI rocks!"
-    };
-
-    const newEvent = await API.graphql(graphqlOperation(addTodo, todoDetails));
-    alert(JSON.stringify(newEvent));
-  };
-
-  listQuery = async () => {
-    console.log("listing todos");
-    const allTodos = await API.graphql(graphqlOperation(listTodos));
-    alert(JSON.stringify(allTodos));
-  };
+  }
+  //uploadFile = evt => {
+  //  const file = evt.target.files[0];
+  //  const name = file.name;
+  //
+  //  Storage.put(name, file).then(() => {
+  //    this.setState({ file: name });
+  //  });
+  //};
+  //
+  //todoMutation = async () => {
+  //  const todoDetails = {
+  //    name: "Party tonight!",
+  //    description: "Amplify CLI rocks!"
+  //  };
+  //
+  //  const newEvent = await API.graphql(graphqlOperation(addTodo, todoDetails));
+  //  alert(JSON.stringify(newEvent));
+  //};
+  //
+  //listQuery = async () => {
+  //  console.log("listing todos");
+  // const allTodos = await API.graphql(graphqlOperation(listTodos));
+  //  alert(JSON.stringify(allTodos));
+  //};
+  //<button onClick={this.listQuery}>GraphQL Query</button>
+  //<button onClick={this.todoMutation}>GraphQL Mutation</button>
 
   render() {
     return (
       <div className="App">
         <p> Pick a file</p>
         <input type="file" onChange={this.uploadFile} />
-        <button onClick={this.listQuery}>GraphQL Query</button>
-        <button onClick={this.todoMutation}>GraphQL Mutation</button>
+        {this.state.videos.map((video, index) => (
+          <p key={index}>{video.title}</p>
+        ))}
         <S3Album level="private" path="" />
       </div>
     );
